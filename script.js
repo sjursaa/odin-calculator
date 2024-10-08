839733bb44affc0d06bab7d573088bb731268682
Sjur <sjursaa@gmail.com>
2024-10-08 20:00:27 +0200

    comment out unused tests

--- script.js
+++ script.js
@@ -30,21 +30,21 @@ function operate(number1, operator, number2) {
 let displayValue = "42069";
 
 // TODO: Remove this stuff
-let number1 = 2;
-let number2 = 2;
-let operator = "+";
-// testing all operators
-let test = operate(number1, operator, number2);
-console.log(test);
-operator = "-";
-test = operate(number1, operator, number2);
-console.log(test);
-operator = "*";
-test = operate(number1, operator, number2);
-console.log(test);
-operator = "/";
-test = operate(number1, operator, number2);
-console.log(test);
+// let number1 = 2;
+// let number2 = 2;
+// let operator = "+";
+// // testing all operators
+// let test = operate(number1, operator, number2);
+// console.log(test);
+// operator = "-";
+// test = operate(number1, operator, number2);
+// console.log(test);
+// operator = "*";
+// test = operate(number1, operator, number2);
+// console.log(test);
+// operator = "/";
+// test = operate(number1, operator, number2);
+// console.log(test);
 
 // setting display-div to the value contained in the script
 function updateDisplay() {
