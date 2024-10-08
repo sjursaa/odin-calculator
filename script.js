9f3be999cac54e8f5105b51d02251f9fbd059687
Sjur <sjursaa@gmail.com>
2024-10-08 15:41:47 +0200

    implement basic solution for operate function

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
-console.log("Hello world");
+function operate(number1, operator, number2) {
+  if (operator == "+") {
+    return number1 + number2;
+  }
+  if (operator == "-") {
+    return number1 - number2;
+  }
+  if (operator == "*") {
+    return number1 * number2;
+  }
+  if (operator == "/") {
+    return number1 / number2;
+  }
+}
+
+let test = operate(1, "+", 3);
+console.log(test);
