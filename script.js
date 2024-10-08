ebfceb1f95a80c20d4ec5fcd0cc1fd27f78393dd
Sjur <sjursaa@gmail.com>
2024-10-08 15:46:42 +0200

    implement functions for each of the operations, call functions in the operate function

--- script.js
+++ script.js
@@ -1,17 +1,34 @@
+function add(number1, number2) {
+  return number1 + number2;
+}
+function subtract(number1, number2) {
+  return number1 - number2;
+}
+function multiply(number1, number2) {
+  return number1 * number2;
+}
+function divide(number1, number2) {
+  return number1 / number2;
+}
+
 function operate(number1, operator, number2) {
   if (operator == "+") {
-    return number1 + number2;
+    return add(number1, number2);
   }
   if (operator == "-") {
-    return number1 - number2;
+    return subtract(number1, number2);
   }
   if (operator == "*") {
-    return number1 * number2;
+    return multiply(number1, number2);
   }
   if (operator == "/") {
-    return number1 / number2;
+    return divide(number1, number2);
   }
 }
 
-let test = operate(1, "+", 3);
+let number1 = 1;
+let number2 = 1;
+let operator = "+";
+
+let test = operate(number1, operator, number2);
 console.log(test);
